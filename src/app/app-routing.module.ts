cdbc10c81601b5d0c345666f0cdeb131506aac8e
Fernando Moura <fer.moura.tech@gmail.com>
2024-01-20 00:11:45 -0300

    pegando os arquivo da Fernada-Kipper

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [
-  // {
-  //   path: "usuario",
-  //   component: ViewUsuario
-  // },
-];
+
+const routes: Routes = [];
 
 @NgModule({
-  //imports: [RouterModule.forRoot(routes)],
-  //exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,8 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-// import { FilterUnitsService } from 'src/app/services/filter-units.service';
-// import { GetUnitsService } from 'src/app/services/get-units.service';
-// import { Location } from 'src/app/types/location.interface';
+import { FilterUnitsService } from 'src/app/services/filter-units.service';
+import { GetUnitsService } from 'src/app/services/get-units.service';
+import { Location } from 'src/app/types/location.interface';
 
 @Component({
   selector: 'app-forms',
@@ -15,26 +15,26 @@ export class FormsComponent implements OnInit {
   filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
-  constructor(){}
-    //private formBuilder: FormBuilder,
-    //private unitService: GetUnitsService,
-    //private filterUnitsService: FilterUnitsService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private unitService: GetUnitsService,
+    private filterUnitsService: FilterUnitsService) { }
 
   ngOnInit(): void {
-    // this.formGroup = this.formBuilder.group({
-    //   hour: '',
-    //   showClosed: true
-    // })
-    // this.unitService.getAllUnits().subscribe(data => {
-    //   this.results = data;
-    //   this.filteredResults = data;
-    // });
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: true
+    })
+    this.unitService.getAllUnits().subscribe(data => {
+      this.results = data;
+      this.filteredResults = data;
+    });
   }
 
   onSubmit(): void {
     let { showClosed, hour } = this.formGroup.value
-    // this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour);
-    // this.unitService.setFilteredUnits(this.filteredResults);
+    this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour);
+    this.unitService.setFilteredUnits(this.filteredResults);
 
     this.submitEvent.emit();
   }

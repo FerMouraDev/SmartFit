69d4f6bf664a37f15428629b9dbba42cf9ecad42
Fernando Moura <fer.moura.tech@gmail.com>
2024-01-19 22:39:16 -0300

    teste comiit

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,24 +1,46 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+// import { FilterUnitsService } from 'src/app/services/filter-units.service';
+// import { GetUnitsService } from 'src/app/services/get-units.service';
+// import { Location } from 'src/app/types/location.interface';
+
 @Component({
-    selector: 'app-forms',
-    templateUrl: './forms.component.html',
-    styleUrls: ['./forms.component.scss']
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit {
-    results = [];
-    formGroup!: FormGroup;
-    constructor(private formBuilder: FormBuilder) {}
-    ngOnInit(): void {
-        this.formGroup = this.formBuilder.group({
-            hour: '',
-            showClosed: false
-        });
-    }
-    onSubmit(): void {
-        console.log("submit");
-    }
-    onClean(): void {
-        //this.re
-    }
+  @Output() submitEvent = new EventEmitter();
+  results: Location[] = [];
+  filteredResults: Location[] = [];
+  formGroup!: FormGroup;
+
+  constructor(){}
+    //private formBuilder: FormBuilder,
+    //private unitService: GetUnitsService,
+    //private filterUnitsService: FilterUnitsService) { }
+
+  ngOnInit(): void {
+    // this.formGroup = this.formBuilder.group({
+    //   hour: '',
+    //   showClosed: true
+    // })
+    // this.unitService.getAllUnits().subscribe(data => {
+    //   this.results = data;
+    //   this.filteredResults = data;
+    // });
+  }
+
+  onSubmit(): void {
+    let { showClosed, hour } = this.formGroup.value
+    // this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour);
+    // this.unitService.setFilteredUnits(this.filteredResults);
+
+    this.submitEvent.emit();
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
+
 }
